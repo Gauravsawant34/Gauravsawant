be9d0c57fbd6a3b926e522bca4e489e018aed495
Gauravsawant34 <gauravsawant3198@gmail.com>
2025-11-13 18:07:46 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -47,6 +47,15 @@ const projects = [
     Previewlink: " https://gauravsawant34.github.io/Mussica-Drumkit/",
     Githublink: " https://gauravsawant34.github.io/Mussica-Drumkit/",
   }, 
+  {
+    title: "Calculator",
+    cardImage: "assets/images/project-page/musiccadrumkit.jpg",
+    description: "The Calculator project is interactive web developement project that allows leraners to Practice their HtML cSS ,and Javascript skills by creating calculator.",
+    tagimg:
+      "https://img.freepik.com/premium-vector/drum-set-music-cartoon-icon-illustration-music-instrument-icon-concept-isolated-premium-flat-cartoon-style_138676-1601.jpg?w=740",
+    Previewlink: " https://gauravsawant34.github.io/Calculator-cs/",
+    Githublink: " https://gauravsawant34.github.io/Calculator-cs/",
+  }, 
 ];
 
 // function for rendering project cards data
